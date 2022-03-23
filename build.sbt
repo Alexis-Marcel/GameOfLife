8891736ca8913c3fdeb796d13c9d34ff5617486d
RICHIER Marcus <marcus.richier8@etu.univ-lorraine.fr>
2022-03-23 18:58:53 +0100

    Ajout d'un test

---
+++ build.sbt
@@ -0,0 +1,7 @@
+name := "JeuDeLaVie"
+
+version := "0.1"
+
+scalaVersion := "2.13.8"
+
+libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.8" % Test
--- src/test/scala/TestJeuDeLaVie.scala
+++ src/test/scala/TestJeuDeLaVie.scala
@@ -1,5 +1,11 @@
 import JeuDeLaVie._;
+import org.scalatest.FunSuite;
 
-object TestJeuDeLaVie {
-
+class TestJeuDeLaVie extends FunSuite {
+  test("chainesToGrille.normal") {
+    val liste = List("XX ", " XX", " X ");
+    val res = chainesToGrille(liste)
+    val exp = List((0,0), (0,1), (1,1), (1,2), (2,1))
+    assert(res === exp)
+  }
 }
