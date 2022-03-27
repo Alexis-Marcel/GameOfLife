d94646bf854e936fca474c9741693d87e33850a3
Alexis MARCEL <91008950+WeriloZzz@users.noreply.github.com>
2022-03-27 18:21:35 +0200

    Continuation des tests

--- src/test/scala/TestJeuDeLaVie.scala
+++ src/test/scala/TestJeuDeLaVie.scala
@@ -79,6 +79,7 @@ class TestJeuDeLaVie extends FunSuite {
     val res = survivantes(l)
     val exp = List((0,1), (1,2), (2,1))
     assert(res === exp)
+    assert(res.length <= l.length)
   }
 
   /*
@@ -105,6 +106,81 @@ class TestJeuDeLaVie extends FunSuite {
     val exp = List((0,2),(1,0))
 
     assert(res === exp)
+  }
+
+  /*
+   * Question 8
+  */
+
+  test("voisines4.normal") {
+    val exp2 = List((-1,0), (0,-1), (0,1), (1,0))
+    val res2 = voisines4( 0 , 0)
+    assert(res2 === exp2)
+    assert(res2.length == 4)
+
+    val exp  = List((-7,-1), (-6,-2), (-6,0), (-5,-1))
+    val res = voisines4( -6 , -1)
+    assert(res === exp)
+    assert(res.length == 4)
+  }
+
+  test("voisines4.grandOuPetit") {
+    intercept[IndexOutOfBoundsException](voisines4( Integer.MAX_VALUE , 0))
+    intercept[IndexOutOfBoundsException](voisines4( 0 , Integer.MAX_VALUE))
+    intercept[IndexOutOfBoundsException](voisines4( Integer.MIN_VALUE , 0))
+    intercept[IndexOutOfBoundsException](voisines4( 0, Integer.MIN_VALUE ))
+    intercept[IndexOutOfBoundsException](voisines4( Integer.MAX_VALUE, Integer.MIN_VALUE ))
+  }
+
+  /*
+   * Question 9
+  */
+
+  test("naitJDLV.normal") {
+
+    assert(naitJDLV(3) === true)
+    assert(naitJDLV(2) === false)
+    assert(naitJDLV(4) === false)
+    assert(naitJDLV(-3) === false)
+
+  }
+
+  test("survitJDLV.normal") {
+
+    assert(survitJDLV(3) === true)
+    assert(survitJDLV(2) === true)
+    assert(survitJDLV(1) === false)
+    assert(survitJDLV(4) === false)
+    assert(survitJDLV(-3) === false)
+
+  }
+
+  test("naitFredkin.normal") {
+
+    assert(naitFredkin(3) === true)
+    assert(naitFredkin(2) === false)
+    assert(naitFredkin(1) === true)
+    assert(naitFredkin(4) === false)
+    assert(naitFredkin(-3) === true)
+
+    //Fredkin : naissance ou survit sont contrains au même règle
+    assert(naitFredkin(2) == survitFredkin(2))
+    assert(naitFredkin(3) == survitFredkin(3))
+
+  }
+
+  test("survitFredkin.normal") {
+
+    assert(survitFredkin(3) === true)
+    assert(survitFredkin(2) === false)
+    assert(survitFredkin(1) === true)
+    assert(survitFredkin(4) === false)
+    assert(survitFredkin(-3) === true)
+
+    //Fredkin : naissance ou survit sont contrains au même règle
+    assert(naitFredkin(2) == survitFredkin(2))
+    assert(naitFredkin(3) == survitFredkin(3))
+
   }
 
 
