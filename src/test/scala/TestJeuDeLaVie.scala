81315d619b7bafb996bad001c176299f7bdb38f3
RICHIER Marcus <marcus.richier8@etu.univ-lorraine.fr>
2022-03-27 13:42:17 +0200

    Test question 2

--- src/test/scala/TestJeuDeLaVie.scala
+++ src/test/scala/TestJeuDeLaVie.scala
@@ -21,4 +21,30 @@ class TestJeuDeLaVie extends FunSuite {
     assert(res === exp)
   }
 
+  /*
+   * Question 2
+  */
+
+  test("coinMinMax.normal") {
+    val liste = List((0,0), (0,1),(0,5), (1,1), (1,2), (2,0), (2,4))
+    val exp  =((0,0), (2,5))
+    val res = coinMinMax(liste)
+    assert(res === exp)
+  }
+
+  test("coinMinMax.inverse") {
+    val liste = List((0,0), (0,1),(0,5), (1,1), (1,2), (2,0), (2,4)).reverse
+    val exp  =((0,0), (2,5))
+    val res = coinMinMax(liste)
+    assert(res === exp)
+  }
+
+  test("coinMinMax.grand") {
+    val liste = List((0,0), (0,1),(0,5), (1,1), (1,2), (2,0), (2,4), (Integer.MAX_VALUE, Integer.MIN_VALUE), (Integer.MIN_VALUE, Integer.MAX_VALUE) )
+    val exp  =((Integer.MIN_VALUE,Integer.MIN_VALUE), (Integer.MAX_VALUE,Integer.MAX_VALUE))
+    val res = coinMinMax(liste)
+    assert(res === exp)
+  }
+  
+
 }
