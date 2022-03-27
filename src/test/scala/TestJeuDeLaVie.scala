270168e9d8eca7f2904f5693217e0b5361725ab9
RICHIER Marcus <marcus.richier8@etu.univ-lorraine.fr>
2022-03-27 13:34:54 +0200

    Ajout d'un test Question 1

--- src/test/scala/TestJeuDeLaVie.scala
+++ src/test/scala/TestJeuDeLaVie.scala
@@ -2,10 +2,23 @@ import JeuDeLaVie._;
 import org.scalatest.FunSuite;
 
 class TestJeuDeLaVie extends FunSuite {
+
+  /*
+   * Question 1
+   */
+
   test("chainesToGrille.normal") {
     val liste = List("XX ", " XX", " X ");
     val res = chainesToGrille(liste)
     val exp = List((0,0), (0,1), (1,1), (1,2), (2,1))
     assert(res === exp)
   }
+
+  test("chainesToGrille.grand") {
+    val liste = List("XX   X", " XX   ", "X   X ");
+    val res = chainesToGrille(liste)
+    val exp = List((0,0), (0,1),(0,5), (1,1), (1,2), (2,0), (2,4))
+    assert(res === exp)
+  }
+
 }
